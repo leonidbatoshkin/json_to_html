77b3be198f0187104286e09812b993cd4e4e2b2d
Leonid Batoshkin <leo.batoshkin@yandex.ru>
2019-03-23 23:09:28 +0300

    add: task2

--- pj.py
+++ pj.py
@@ -9,3 +9,9 @@ def simple_convert(file):
         key = elem['title']
         value = elem['body']
         print(f'<h1>{key}</h1><p>{value}</p>', end='')
+
+
+def full_convert(file):
+    for elem in file:
+        for key, value in elem.items():
+            print(f'<{key}>{value}</{key}>', end='')
