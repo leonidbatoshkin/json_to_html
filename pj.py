b048a15fe2a473a5e1a09e33d7d4b9c640792667
Leonid Batoshkin <leo.batoshkin@yandex.ru>
2019-03-24 19:46:30 +0300

    change: print to return output

--- pj.py
+++ pj.py
@@ -3,40 +3,47 @@ import re
 
 with open("source.json", "r") as read_file:
     data = json.load(read_file)
+    result = ''
 
 
 def simple_convert(file):
+    global result
     for elem in file:
         key = elem['title']
         value = elem['body']
-        print(f'<h1>{key}</h1><p>{value}</p>', end='')
+        result += f'<h1>{key}</h1><p>{value}</p>'
+    return result
 
 
 def full_convert(file):
+    global result
     for key, value in file.items():
-        print(f'<{key}>{value}</{key}>', end='')
+        result += f'<{key}>{value}</{key}>'
+    return result
 
 
 def make_list(file):
-    print('<ul>', end='')
+    global result
+    result += '<ul>'
     for elem in file:
-        print('<li>', end='')
+        result += '<li>'
         for key, value in elem.items():
             if type(value) is list:
-                print(f'<{key}>', end='')
+                result += f'<{key}>'
                 make_list(value)
-                print(f'</{key}>', end='')
+                result += f'</{key}>'
             else:
-                print(f'<{key}>{value}</{key}>', end='')
-        print('</li>', end='')
-    print('</ul>', end='')
+                result += f'<{key}>{value}</{key}>'
+        result += '</li>'
+    result += '</ul>'
+    return result
 
 
 def combine(file):
     if type(file) is list:
-        make_list(file)
+        return make_list(file)
     else:
-        full_convert(file)
+        return full_convert(file)
 
 
 def raw_html(code: str):
@@ -44,6 +51,7 @@ def raw_html(code: str):
 
 
 def make_selector(file):
+    global result
     pattern_id = r'#[\w-]+'
     pattern_cls = r'\.[\w-]+'
 
@@ -55,8 +63,9 @@ def make_selector(file):
         render_cls = [tag[1:] for tag in _cls]
 
         if render_id and not render_cls:
-            print('<{0} id="{1}">{2}</{3}>'.format(tg, ' '.join(render_id), raw_html(value), tg), end='')
+            result += '<{0} id="{1}">{2}</{3}>'.format(tg, ' '.join(render_id), raw_html(value), tg)
         if render_cls and not render_id:
-            print('<{0} class="{1}">{2}</{3}>'.format(tg, ' '.join(render_cls), raw_html(value), tg), end='')
+            result += '<{0} class="{1}">{2}</{3}>'.format(tg, ' '.join(render_cls), raw_html(value), tg)
         else:
-            print('<{0} id="{1}" class="{2}">{3}</{4}>'.format(tg, ' '.join(render_id), ' '.join(render_cls), raw_html(value), tg), end='')
+            result += '<{0} id="{1}" class="{2}">{3}</{4}>'.format(tg, ' '.join(render_id), ' '.join(render_cls), raw_html(value), tg)
+    return result
